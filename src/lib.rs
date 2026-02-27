714a831f8e47d6c3e7903c1584133f8f946c1aa4
redshiftzero <jen@relicveil.com>
2026-02-27 17:20:24 -0500

    compressed ristretto group element: to_bytes, as_bytes, eq, ct_eq

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,35 @@
-/// A Ristretto encoding.
-#[derive(Copy, Clone, Eq, PartialEq, Hash)]
+//! Ristretto255 is an implementation of the Ristretto255 prime-order group
+//! based on the libcrux family of crates.
+//! 
+//! It is designed to be a drop-in replacement for the `curve25519-dalek` crate.
+
+use subtle::{Choice, ConstantTimeEq};
+
+/// A compressed Ristretto255 point.
+#[derive(Copy, Clone, Eq, Hash)]
 pub struct CompressedRistretto(pub [u8; 32]);
+
+impl PartialEq for CompressedRistretto {
+    fn eq(&self, other: &Self) -> bool {
+        self.0.ct_eq(&other.0).into()
+    }
+}
+
+impl ConstantTimeEq for CompressedRistretto {
+    fn ct_eq(&self, other: &CompressedRistretto) -> Choice {
+        self.as_bytes().ct_eq(other.as_bytes())
+    }
+}
+
+impl CompressedRistretto {
+    /// Copy the bytes of this `CompressedRistretto`.
+    pub const fn to_bytes(&self) -> [u8; 32] {
+        self.0
+    }
+
+    /// View this `CompressedRistretto` as an array of bytes.
+    pub const fn as_bytes(&self) -> &[u8; 32] {
+        &self.0
+    }
+}
+
