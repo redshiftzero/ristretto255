036d49cc4b5a73a67692656844bff324d1c19518
redshiftzero <jen@relicveil.com>
2026-04-20 14:04:34 -0400

    bench: scalar multiplication

--- ristretto255/benches/ristretto.rs
+++ ristretto255/benches/ristretto.rs
@@ -28,11 +28,27 @@ fn bench_decompress_dalek(c: &mut Criterion) {
     });
 }
 
+fn bench_scalar_mul_ours(c: &mut Criterion) {
+    let bp = ristretto255::RistrettoPoint::basepoint();
+    let k = ristretto255::Scalar::from_bytes_mod_order(&[7u8; 32]);
+    c.bench_function("ristretto255 variable-base scalar mul", |b| {
+        b.iter(|| &k * &bp)
+    });
+}
+
+fn bench_scalar_mul_dalek(c: &mut Criterion) {
+    let bp = curve25519_dalek::constants::RISTRETTO_BASEPOINT_POINT;
+    let k = curve25519_dalek::scalar::Scalar::from_bytes_mod_order([7u8; 32]);
+    c.bench_function("dalek variable-base scalar mul", |b| b.iter(|| &k * &bp));
+}
+
 criterion_group!(
     benches,
     bench_compress_ours,
     bench_compress_dalek,
     bench_decompress_ours,
     bench_decompress_dalek,
+    bench_scalar_mul_ours,
+    bench_scalar_mul_dalek,
 );
 criterion_main!(benches);
